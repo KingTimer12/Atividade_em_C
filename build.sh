03aa4e9064d0f0711794ec46918cc0a60bfc0934
kingtimer12 <queentimer12@gmail.com>
2024-09-24 09:58:37 -0300

    chore: Atualização no script de build para OS Unix (Não tem nada a ver com a atividade)

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in 'questao1' 'questao2' 'questao3' 'questao4'
+for i in 'questao1' 'questao2' 'questao3' 'questao4' 'questao5' 'questao6' 'questao7' 'questao8'
 do
 cd "$i/"
 bash build.sh
